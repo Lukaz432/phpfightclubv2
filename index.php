5bb477f39e91a210815b3c982c2fb7b75119fb2b
Lukaz432 <burgerkingz234@gmail.com>
2019-10-18 17:02:47 +0300

    commitv1

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+$array = [
+    [
+        'owner' => 'Petras Pizdis',
+        'item' => 'telefonas',
+        'status' => 'prapistas',
+    ],
+    [
+        'owner' => 'Tomas Ablomas',
+        'item' => 'buljonas',
+        'status' => 'naudojamas',   
+    ],
+    [
+        'owner' => 'Ana Shitkova',
+        'item' => 'rulonas',
+        'status' => 'pasibaiges',
+    ],
+ ];
+ var_dump($array);
+
+
+?>
+<html>
+    <head>
+        <title>Class Work</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
