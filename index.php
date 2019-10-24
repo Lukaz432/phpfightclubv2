49bfc09340cba2437a0ea425265c621f277e80fa
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2019-10-24 16:26:38 +0300

    alcohol_shop_array

--- index.php
+++ index.php
@@ -1,54 +1,35 @@
 <?php
 
-$zodziai = [
-    'Lukas',
-    'pareigunas',
-    'moteris', 
-    'vyras',
-    'naktis', 
-    'prietema',
-    'saulelydis',
-    'vidurnaktis',
-    'bego',
-    'pasove',
-    'pamylejo',
-    'isjunge',
-    'automobiliu',
-    'motociklu',
-    'irankiu',
-    'ranka',
-    'sankryzoje',
-    'namuose',
-    'greitkelyje',
-    'televizijos_bokstas',
-    'greitai',
-    'apsvaigusiai',
-    'tingiai',
-    'mikliai'
-    ];
-
-
-for ($i = 0; $i <  rand(100,300); $i++) {
-    $random_word = $zodziai[(rand(0, count($zodziai) - 1))];
-    $rasinys .= " $random_word";
-}
-
-
-$h1 = 'rasinys';
-
-
+$drinks = [
+    [
+       'name' => 'Vilkmerges Alus',
+       'price_stock' => 2.5,
+       'discount' => 0,
+       'img' => 'https://www.barbora.lt/api/Images/GetInventoryImage?id=7acd8bad-f09a-470c-9646-e134ddeee5d7',
+    ],
+    [
+       'name' => 'Kalnapilio Alus',
+       'price_stock' => 2.5,
+       'discount' => 5,
+       'img' => 'http://intermarket.lt/image/cache/catalog/Alkoholiniai%20g%C4%97rimai,%20cigaret%C4%97s/Alus/_DSC1678%20as%20Smart%20Object-1%20copy-1000x1000.png',
+    ],
+    [
+       'name' => 'Raudonu Plytu Alus',
+       'price_stock' => 3.0,
+       'discount' => 10,
+       'img' => 'https://www.barbora.lt/api/Images/GetInventoryImage?id=2a6b2198-e0ae-4bc8-afea-13d4ef200809',
+    ],
+    [
+       'name' => 'Volfas Engerlman Alus',
+       'price_stock' => 3.5,
+       'discount' => 15,
+       'img' => 'https://www.barbora.lt/api/Images/GetInventoryImage?id=790702e3-2daf-46b0-8694-798933ebb539',
+    ],
+    [
+       'name' => 'Heineken Alus',
+       'price_stock' => 4.0,
+       'discount' => 20,
+       'img' => 'https://us.123rf.com/450wm/ultimagaina/ultimagaina1603/ultimagaina160300031/55062044-usa-imported-heineken-beer-bottle-heineken-is-a-popular-dutch-pale-lager-beer-produced-by-the-dutch-.jpg?ver=6',
+    ],  
+];
 ?>
-<html>
-    <head>
-        <title>Class Work</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <h1><?php print $h1 ?></h1>
-        <ul>
-            <li>
-                <?php print $rasinys ?>
-            </li>
-        </ul>
-    </body>
-</html>
